54bd553c31c53ee8454a04862060599aa92a806c
noah <m3252@naver.com>
2023-01-01 22:57:31 +0900

    feat: 큐 구현

---
+++ src/main/java/structure/queue/MyQueue.java
@@ -0,0 +1,72 @@
+package structure.queue;
+
+import java.util.NoSuchElementException;
+
+public class MyQueue<T> {
+    class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node<T> first;
+    private Node<T> last;
+
+    public void add(T item) {
+        Node<T> t = new Node<>(item);
+        if (last != null) {
+            last.next = t;
+        }
+        last = t;
+        if (first == null) {
+            first = last;
+        }
+    }
+
+    public T remove() {
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+
+        T data = first.data;
+        first = first.next;
+
+        if (first == null) {
+            last = null;
+        }
+        return data;
+    }
+
+    public T peek() {
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public static void main(String[] args) {
+        MyQueue<Integer> queue = new MyQueue<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.peek());
+        System.out.println(queue.remove());
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.remove());
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.remove());
+    }
+
+
+}
+
